14c37afb2d800e2d5b70610e2fe9875060b54b1f
Benjamin Fichot <fichot.benjamin@gmail.com>
2022-11-16 00:11:56 +0100

    mise en place stack back java spring boot + dependences

---
+++ src/main/java/wf3/project/alpha_betise/AlphaBetiseApplication.java
@@ -0,0 +1,13 @@
+package wf3.project.alpha_betise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaBetiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaBetiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wf3/project/alpha_betise/AlphaBetiseApplicationTests.java
@@ -0,0 +1,13 @@
+package wf3.project.alpha_betise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaBetiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
