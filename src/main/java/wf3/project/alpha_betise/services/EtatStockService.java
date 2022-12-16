e2a42c194c36c97ec35d76eb6251400cbc69a484
Benjamin Fichot <fichot.benjamin@gmail.com>
2022-12-16 16:50:56 +0100

    6-creation-entity-EtatStock

---
+++ src/main/java/wf3/project/alpha_betise/services/EtatStockService.java
@@ -0,0 +1,16 @@
+package wf3.project.alpha_betise.services;
+
+import java.util.List;
+
+import wf3.project.alpha_betise.entities.EtatStock;
+
+public interface EtatStockService {
+
+	public List<EtatStock> getAll();
+
+	public EtatStock get(Integer id) throws Exception;
+
+	public EtatStock post(EtatStock etatStock);
+
+	public void delete(Integer id);
+}
