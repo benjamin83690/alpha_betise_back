7a90f97ea58dacbc1ec4bb33ef9cb06c1ca64bdb
Arnaud Costard <arnaud_costard@hotmail.fr>
2022-12-16 15:04:20 +0100

    4-creation-entity-Editeur

---
+++ src/main/java/wf3/project/alpha_betise/repositories/EditeurRepository.java
@@ -0,0 +1,10 @@
+package wf3.project.alpha_betise.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wf3.project.alpha_betise.entities.Editeur;
+
+@Repository
+public interface EditeurRepository extends JpaRepository<Editeur, Integer> {
+
+}
