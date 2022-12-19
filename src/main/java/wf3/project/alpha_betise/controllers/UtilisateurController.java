ada357917b9a122c445117bf78f343695d93a7ac
Benjamin Fichot <fichot.benjamin@gmail.com>
2022-12-19 23:49:10 +0100

    22-creation-entity-utilisateur

---
+++ src/main/java/wf3/project/alpha_betise/controllers/UtilisateurController.java
@@ -0,0 +1,42 @@
+package wf3.project.alpha_betise.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import wf3.project.alpha_betise.entities.Utilisateur;
+import wf3.project.alpha_betise.services.UtilisateurService;
+
+@RestController
+@RequestMapping("/utilisateur")
+public class UtilisateurController {
+
+	@Autowired
+	private UtilisateurService utilisateurService;
+
+	@GetMapping("/all")
+	public List<Utilisateur> getAll() {
+		return utilisateurService.getAll();
+	}
+
+	@GetMapping("/{id}")
+	public Utilisateur get(@PathVariable("id") Integer id) throws Exception {
+		return utilisateurService.get(id);
+	}
+
+	@PostMapping
+	public Utilisateur post(Utilisateur utilisateur) {
+		return utilisateurService.post(utilisateur);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") Integer id) {
+		utilisateurService.delete(id);
+	}
+}
