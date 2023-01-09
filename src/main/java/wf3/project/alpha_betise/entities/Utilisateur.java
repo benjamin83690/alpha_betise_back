403659aff6d76a79e0374b5a5afb1b95e751738e
Benjamin Fichot <fichot.benjamin@gmail.com>
2023-01-09 15:39:46 +0100

    35-relations-entity-utilisateur

--- src/main/java/wf3/project/alpha_betise/entities/Utilisateur.java
+++ src/main/java/wf3/project/alpha_betise/entities/Utilisateur.java
@@ -1,9 +1,19 @@
 package wf3.project.alpha_betise.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -21,16 +31,31 @@ public class Utilisateur {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@NonNull
+	@Column(name = "utilisateur_id")
 	private Integer id;
+
 	@NonNull
 	private String nom;
+
 	@NonNull
 	private String prenom;
+
 	@NonNull
 	private String email;
+
 	@NonNull
 	private String motDePasse;
+
 	@NonNull
 	private Short age;
+
 	private String photo;
+	
+	@ManyToOne
+	@JoinColumn(name = "role_id", nullable = false)
+	private Role role;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "evenements_utilisateur", joinColumns = @JoinColumn(name = "utilisateur_id"), inverseJoinColumns = @JoinColumn(name = "evenement_id"))
+	private List<Evenement> evenementsUtilisateur = new ArrayList<Evenement>();
 }
