7e8dd0ca8b3e2aea700b8682b8b0a23e2bf25709
Benjamin Fichot <fichot.benjamin@gmail.com>
2023-01-09 17:42:16 +0100

    39-relations-entity-utilisateur-suite

--- src/main/java/wf3/project/alpha_betise/entities/Commande.java
+++ src/main/java/wf3/project/alpha_betise/entities/Commande.java
@@ -4,9 +4,12 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -44,4 +47,8 @@ public class Commande {
 	@NonNull
 	@Column(name = "adresse_facturation")
 	private String adresseFacturation;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "utilisateur_id")
+	private Utilisateur utilisateurId;
 }
