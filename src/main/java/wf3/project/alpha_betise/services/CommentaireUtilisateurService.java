abb4780cdac378864f6a73173a609d633df1d7da
xAm64 <48089283+xAm64@users.noreply.github.com>
2023-01-09 11:19:24 +0100

    25-creation-entity-commentaireutilisateur, créer CommentaireUtilisateur

---
+++ src/main/java/wf3/project/alpha_betise/services/CommentaireUtilisateurService.java
@@ -0,0 +1,15 @@
+package wf3.project.alpha_betise.services;
+
+import java.util.List;
+import wf3.project.alpha_betise.entities.CommentaireUtilisateur;
+
+public interface CommentaireUtilisateurService {
+
+	public List<CommentaireUtilisateur> getAll();
+
+	public CommentaireUtilisateur get(Integer id) throws Exception;
+
+	public CommentaireUtilisateur post(CommentaireUtilisateur commentaireUtilisateur);
+
+	public void delete(Integer id);
+}
