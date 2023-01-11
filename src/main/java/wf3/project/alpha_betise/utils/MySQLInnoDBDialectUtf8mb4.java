06dcc9f5f219454f4a2f87ab79cdd8e9faada538
Arnaud Costard <arnaud_costard@hotmail.fr>
2023-01-11 17:38:38 +0100

    Mise à jour properties pdû àoner en version du SQLdprojet

--- src/main/java/wf3/project/alpha_betise/utils/MySQLInnoDBDialectUtf8mb4.java
+++ src/main/java/wf3/project/alpha_betise/utils/MySQLInnoDBDialectUtf8mb4.java
@@ -6,6 +6,6 @@ public class MySQLInnoDBDialectUtf8mb4 extends MySQLDialect {
 
     @Override
     public String getTableTypeString() {
-        return "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci";
+        return "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci";
     }
 }
